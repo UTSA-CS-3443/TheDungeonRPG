12b183aa49d878af61e570177c3a7dcfa31bd3ff
oyr380 <oyr380@my.utsa.edu>
2021-11-28 17:10:15 -0600

    Inventory controller added

--- src/application/controller/InventoryController.java
+++ src/application/controller/InventoryController.java
@@ -1,6 +1,7 @@
 package application.controller;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import application.Main;
@@ -32,18 +33,11 @@ public class InventoryController implements EventHandler<ActionEvent>, Initializ
     @FXML private Button item8;
     @FXML private Button item9;
     @FXML private Button item10;
-
     
-	public void initialize(URL location, ResourceBundle resources) {
-
-		playerName.setText(Main.playerChar.getName());
-		playerLevel.setText(Integer.toString(Main.playerChar.getLevel()));
-		playerHP.setText(Integer.toString(Main.playerChar.getCurrHealth()));
-		
-		
-		//TODO
-		//FIXME
-		//Check inventory to initialize these (arraylist?)
+    /**
+     * Method that initializes all TextField objects related to items names to "Empty" 
+     */
+    private void initializeInventoryNames() {
 		item1.setText("Empty");
 		item2.setText("Empty");
 		item3.setText("Empty");
@@ -54,182 +48,413 @@ public class InventoryController implements EventHandler<ActionEvent>, Initializ
 		item8.setText("Empty");
 		item9.setText("Empty");
 		item10.setText("Empty");
-
-
-	
 	}
     
+    /**
+     * Loads the values from ArrayList<String> itemList into the appropriate TextField objects
+     * 
+     * @param itemList - ArrayList<String> itemList with values for TextField objects related to items
+     */
+    private void loadInventoryList( ArrayList<String> itemList ) {
+    	int i = 0;
+    	
+    	for( i = 0; i >= itemList.size() || i >= 10; i++ ) {
+    		switch(i) {
+    			case 0:
+    				item1.setText( itemList.get(i) );
+    				break;
+    			case 1:
+    				item2.setText( itemList.get(i) );
+    				break;
+    			case 2:
+    				item3.setText( itemList.get(i) );
+    				break;
+    			case 3:
+    				item4.setText( itemList.get(i) );
+    				break;
+    			case 4:
+    				item5.setText( itemList.get(i) );
+    				break;
+    			case 5:
+    				item6.setText( itemList.get(i) );
+    				break;
+    			case 6:
+    				item7.setText( itemList.get(i) );
+    				break;
+    			case 7:
+    				item8.setText( itemList.get(i) );
+    				break;
+    			case 8:
+    				item9.setText( itemList.get(i) );
+    				break;
+    			case 9:
+    				item10.setText( itemList.get(i) );
+    				break;
+    			default:
+    				break;
+    		}
+    	}
+    }
+    
+    /* MOVED TO Inventory.java in the useItem() method
+    public void String item ) {
+    	switch( item ) {
+    		case "Potion":
+    			// Add 2 HP
+    			Main.playerChar.setCurrHealth( Main.playerChar.getCurrHealth() + 2 );
+    			break;
+    		case "Mid potion":
+    			// Add 5 HP
+    			Main.playerChar.setCurrHealth( Main.playerChar.getCurrHealth() + 5 );
+    			break;
+    		case "Max potion":
+    			// Add 10 HP
+    			Main.playerChar.setCurrHealth( Main.playerChar.getCurrHealth() + 10 );
+    			break;
+    		case "Strength up":
+    			// Add 1 STR
+    			Main.playerChar.setStr( Main.playerChar.getStr() + 1 );
+    			break;
+    		case "Defense up":
+    			// Add 1 DEF
+    			Main.playerChar.setDef( Main.playerChar.getDef() + 1 );
+    			break;
+    		case "Speed up":
+    			// Add 1 SPD
+    			Main.playerChar.setSpd( Main.playerChar.getSpd() + 1 );
+    			break;
+    		case "Dexterity up":
+    			// Add 1 DEX
+    			Main.playerChar.setDex( Main.playerChar.getDex() + 1 );
+    			break;
+    		default:
+    			// nothing happens ... placeholder
+    			break;
+    	}
+        
+    	// Ensure any changes to not put currHealth higher than maxHealth
+       	if( Main.playerChar.getCurrHealth() > Main.playerChar.getMaxHealth() ) {
+        	Main.playerChar.setCurrHealth(  Main.playerChar.getMaxHealth() );
+        }
+    }
+    */
+    
+    //Javadoc auto-populated due to @Override tag
+    /**
+     * 
+     */
+    @Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+		playerName.setText( Main.playerChar.getName() );
+		playerLevel.setText( "" + Main.playerChar.getLevel() );
+		playerHP.setText( "" + Main.playerChar.getCurrHealth() );
+		// playerHP.setText( Main.playerChar.getCurrHealth() + " / " + Main.playerChar.getMaxHealth() ); // code if max HP is included in same box
+	    curExp.setText( "" + Main.playerChar.getExp() );
+	    nextLevel.setText( "N/A" ); // FIXME: Determine how this will be calculated and displayed
+		
+		// default all item button names to Empty
+		initializeInventoryNames();
+		
+		//set the inventory button names
+		loadInventoryList( Main.playerChar.inventory.getItemList() );
+	}
+    
+	/**
+	 * Method executed when the item1 Button object is pressed
+	 * 
+	 * @param - default ActionEvent object representation of Button object being pressed
+	 */
     @FXML void item1Pressed(ActionEvent event) {
-    	//Do Something
+    	// if not empty
+    	if( !( item1.getText().equals( "Empty" ) ) ) {
+    		
+    		// Use item, apply its effect
+    		Main.playerChar.inventory.useItem( 0 );
+    		
+	    	// Then return To Battle
+	    	try {
+				Parent root = FXMLLoader.load(getClass().getResource("../view/Battle.fxml"));
+				Main.stage.setScene(new Scene(root, 800, 800));
+				Main.stage.setTitle("The Dungeon RPG - FIGHT");
+				Main.stage.show();
+				
+			} 
+	    	
+	    	catch(Exception e) {
+				e.printStackTrace();
+			}
+    	}
     	
-    	//Return To Battle
-    	try {
-			Parent root = FXMLLoader.load(getClass().getResource("../view/Battle.fxml"));
-			Main.stage.setScene(new Scene(root, 800, 800));
-			Main.stage.setTitle("The Dungeon RPG - FIGHT");
-			Main.stage.show();
-			
-		} 
-    	
-    	catch(Exception e) {
-			e.printStackTrace();
-		}
+    	// if empty, do nothing
     }
-
+    
+    /**
+     * Method executed when the item2 Button object is pressed
+	 * 
+     * @param - default ActionEvent object representation of Button object being pressed
+     */
     @FXML void item2Pressed(ActionEvent event) {
-//Do Something
+    	// if not empty
+    	if( !( item2.getText().equals( "Empty" ) ) ) {
+    		
+    		// Use item, apply its effect
+    		Main.playerChar.inventory.useItem( 1 );
+    		
+	    	// Then return To Battle
+	    	try {
+				Parent root = FXMLLoader.load(getClass().getResource("../view/Battle.fxml"));
+				Main.stage.setScene(new Scene(root, 800, 800));
+				Main.stage.setTitle("The Dungeon RPG - FIGHT");
+				Main.stage.show();
+				
+			} 
+	    	
+	    	catch(Exception e) {
+				e.printStackTrace();
+			}
+    	}
     	
-    	//Return To Battle
-    	try {
-    		Parent root = FXMLLoader.load(getClass().getResource("../view/Battle.fxml"));
-			Main.stage.setScene(new Scene(root, 800, 800));
-			Main.stage.setTitle("The Dungeon RPG - FIGHT");
-			Main.stage.show();
-			
-		} 
-    	
-    	catch(Exception e) {
-			e.printStackTrace();
-		}
+    	// if empty, do nothing
     }
-
+    
+    /**
+     * Method executed when the item3 Button object is pressed
+	 * 
+     * @param - default ActionEvent object representation of Button object being pressed
+     */
     @FXML void item3Pressed(ActionEvent event) {
-    	//Do Something
+    	// if not empty
+    	if( !( item3.getText().equals( "Empty" ) ) ) {
+    		
+    		// Use item, apply its effect
+    		Main.playerChar.inventory.useItem( 2 );
+    		
+	    	// Then return To Battle
+	    	try {
+				Parent root = FXMLLoader.load(getClass().getResource("../view/Battle.fxml"));
+				Main.stage.setScene(new Scene(root, 800, 800));
+				Main.stage.setTitle("The Dungeon RPG - FIGHT");
+				Main.stage.show();
+				
+			} 
+	    	
+	    	catch(Exception e) {
+				e.printStackTrace();
+			}
+    	}
     	
-    	//Return To Battle
-    	try {
-    		Parent root = FXMLLoader.load(getClass().getResource("../view/Battle.fxml"));
-			Main.stage.setScene(new Scene(root, 800, 800));
-			Main.stage.setTitle("The Dungeon RPG - FIGHT");
-			Main.stage.show();
-			
-		} 
-    	
-    	catch(Exception e) {
-			e.printStackTrace();
-		}
+    	// if empty, do nothing
     }
-
+    
+    /**
+     * Method executed when the item4 Button object is pressed
+	 * 
+     * @param - default ActionEvent object representation of Button object being pressed
+     */
     @FXML void item4Pressed(ActionEvent event) {
-    	//Do Something
+    	// if not empty
+    	if( !( item4.getText().equals( "Empty" ) ) ) {
+    		
+    		// Use item, apply its effect
+    		Main.playerChar.inventory.useItem( 3 );
+    		
+	    	// Then return To Battle
+	    	try {
+				Parent root = FXMLLoader.load(getClass().getResource("../view/Battle.fxml"));
+				Main.stage.setScene(new Scene(root, 800, 800));
+				Main.stage.setTitle("The Dungeon RPG - FIGHT");
+				Main.stage.show();
+				
+			} 
+	    	
+	    	catch(Exception e) {
+				e.printStackTrace();
+			}
+    	}
     	
-    	//Return To Battle
-    	try {
-    		Parent root = FXMLLoader.load(getClass().getResource("../view/Battle.fxml"));
-			Main.stage.setScene(new Scene(root, 800, 800));
-			Main.stage.setTitle("The Dungeon RPG - FIGHT");
-			Main.stage.show();
-			
-		} 
-    	
-    	catch(Exception e) {
-			e.printStackTrace();
-		}
+    	// if empty, do nothing
     }
-
+    
+    /**
+     * Method executed when the item5 Button object is pressed
+	 * 
+     * @param - default ActionEvent object representation of Button object being pressed
+     */
     @FXML void item5Pressed(ActionEvent event) {
-    	//Do Something
+    	// if not empty
+    	if( !( item5.getText().equals( "Empty" ) ) ) {
+    		
+    		// Use item, apply its effect
+    		Main.playerChar.inventory.useItem( 4 );
+    		
+	    	// Then return To Battle
+	    	try {
+				Parent root = FXMLLoader.load(getClass().getResource("../view/Battle.fxml"));
+				Main.stage.setScene(new Scene(root, 800, 800));
+				Main.stage.setTitle("The Dungeon RPG - FIGHT");
+				Main.stage.show();
+				
+			} 
+	    	
+	    	catch(Exception e) {
+				e.printStackTrace();
+			}
+    	}
     	
-    	//Return To Battle
-    	try {
-    		Parent root = FXMLLoader.load(getClass().getResource("../view/Battle.fxml"));
-			Main.stage.setScene(new Scene(root, 800, 800));
-			Main.stage.setTitle("The Dungeon RPG - FIGHT");
-			Main.stage.show();
-			
-		} 
-    	
-    	catch(Exception e) {
-			e.printStackTrace();
-		}
+    	// if empty, do nothing
     }
     
+    /**
+     * Method executed when the item6 Button object is pressed
+	 * 
+     * @param - default ActionEvent object representation of Button object being pressed
+     */
     @FXML void item6Pressed(ActionEvent event) {
-    	//Do Something
+    	// if not empty
+    	if( !( item6.getText().equals( "Empty" ) ) ) {
+    		
+    		// Use item, apply its effect
+    		Main.playerChar.inventory.useItem( 5 );
+    		
+	    	// Then return To Battle
+	    	try {
+				Parent root = FXMLLoader.load(getClass().getResource("../view/Battle.fxml"));
+				Main.stage.setScene(new Scene(root, 800, 800));
+				Main.stage.setTitle("The Dungeon RPG - FIGHT");
+				Main.stage.show();
+				
+			} 
+	    	
+	    	catch(Exception e) {
+				e.printStackTrace();
+			}
+    	}
     	
-    	//Return To Battle
-    	try {
-    		Parent root = FXMLLoader.load(getClass().getResource("../view/Battle.fxml"));
-			Main.stage.setScene(new Scene(root, 800, 800));
-			Main.stage.setTitle("The Dungeon RPG - FIGHT");
-			Main.stage.show();
-			
-		} 
-    	
-    	catch(Exception e) {
-			e.printStackTrace();
-		}
+    	// if empty, do nothing
     }
     
+    /**
+     * Method executed when the item7 Button object is pressed
+	 * 
+     * @param - default ActionEvent object representation of Button object being pressed
+     */
     @FXML void item7Pressed(ActionEvent event) {
-    	//Do Something
+    	// if not empty
+    	if( !( item7.getText().equals( "Empty" ) ) ) {
+    		
+    		// Use item, apply its effect
+    		Main.playerChar.inventory.useItem( 6 );
+    		
+	    	// Then return To Battle
+	    	try {
+				Parent root = FXMLLoader.load(getClass().getResource("../view/Battle.fxml"));
+				Main.stage.setScene(new Scene(root, 800, 800));
+				Main.stage.setTitle("The Dungeon RPG - FIGHT");
+				Main.stage.show();
+				
+			} 
+	    	
+	    	catch(Exception e) {
+				e.printStackTrace();
+			}
+    	}
     	
-    	//Return To Battle
-    	try {
-    		Parent root = FXMLLoader.load(getClass().getResource("../view/Battle.fxml"));
-			Main.stage.setScene(new Scene(root, 800, 800));
-			Main.stage.setTitle("The Dungeon RPG - FIGHT");
-			Main.stage.show();
-			
-		} 
-    	
-    	catch(Exception e) {
-			e.printStackTrace();
-		}
+    	// if empty, do nothing
     }
     
+    /**
+     * Method executed when the item8 Button object is pressed
+	 * 
+     * @param - default ActionEvent object representation of Button object being pressed
+     */
     @FXML void item8Pressed(ActionEvent event) {
-    	//Do Something
+    	// if not empty
+    	if( !( item8.getText().equals( "Empty" ) ) ) {
+    		
+    		// Use item, apply its effect
+    		Main.playerChar.inventory.useItem( 7 );
+    		
+	    	// Then return To Battle
+	    	try {
+				Parent root = FXMLLoader.load(getClass().getResource("../view/Battle.fxml"));
+				Main.stage.setScene(new Scene(root, 800, 800));
+				Main.stage.setTitle("The Dungeon RPG - FIGHT");
+				Main.stage.show();
+				
+			} 
+	    	
+	    	catch(Exception e) {
+				e.printStackTrace();
+			}
+    	}
     	
-    	//Return To Battle
-    	try {
-    		Parent root = FXMLLoader.load(getClass().getResource("../view/Battle.fxml"));
-			Main.stage.setScene(new Scene(root, 800, 800));
-			Main.stage.setTitle("The Dungeon RPG - FIGHT");
-			Main.stage.show();
-			
-		} 
-    	
-    	catch(Exception e) {
-			e.printStackTrace();
-		}
+    	// if empty, do nothing
     }
     
+    /**
+     * Method executed when the item9 Button object is pressed
+	 * 
+     * @param - default ActionEvent object representation of Button object being pressed
+     */
     @FXML void item9Pressed(ActionEvent event) {
-    	//Do Something
+    	// if not empty
+    	if( !( item9.getText().equals( "Empty" ) ) ) {
+    		
+    		// Use item, apply its effect
+    		Main.playerChar.inventory.useItem( 8 );
+    		
+	    	// Then return To Battle
+	    	try {
+				Parent root = FXMLLoader.load(getClass().getResource("../view/Battle.fxml"));
+				Main.stage.setScene(new Scene(root, 800, 800));
+				Main.stage.setTitle("The Dungeon RPG - FIGHT");
+				Main.stage.show();
+				
+			} 
+	    	
+	    	catch(Exception e) {
+				e.printStackTrace();
+			}
+    	}
     	
-    	//Return To Battle
-    	try {
-    		Parent root = FXMLLoader.load(getClass().getResource("../view/Battle.fxml"));
-			Main.stage.setScene(new Scene(root, 800, 800));
-			Main.stage.setTitle("The Dungeon RPG - FIGHT");
-			Main.stage.show();
-			
-		} 
-    	
-    	catch(Exception e) {
-			e.printStackTrace();
-		}
+    	// if empty, do nothing
     }
-
+    
+	/**
+	 * Method executed when the item10 Button object is pressed
+	 * 
+	 * @param - default ActionEvent object representation of Button object being pressed
+	 */
     @FXML void item10Pressed(ActionEvent event) {
-    	//Do Something
+    	// if not empty
+    	if( !( item10.getText().equals( "Empty" ) ) ) {
+    		
+    		// Use item, apply its effect
+    		Main.playerChar.inventory.useItem( 9 );
+    		
+	    	// Then return To Battle
+	    	try {
+				Parent root = FXMLLoader.load(getClass().getResource("../view/Battle.fxml"));
+				Main.stage.setScene(new Scene(root, 800, 800));
+				Main.stage.setTitle("The Dungeon RPG - FIGHT");
+				Main.stage.show();
+				
+			} 
+	    	
+	    	catch(Exception e) {
+				e.printStackTrace();
+			}
+    	}
     	
-    	//Return To Battle
-    	try {
-    		Parent root = FXMLLoader.load(getClass().getResource("../view/Battle.fxml"));
-			Main.stage.setScene(new Scene(root, 800, 800));
-			Main.stage.setTitle("The Dungeon RPG - FIGHT");
-			Main.stage.show();
-			
-		} 
-    	
-    	catch(Exception e) {
-			e.printStackTrace();
-		}
+    	// if empty, do nothing
     }    
-
-	@Override
+    
+    //Javadoc auto-populated due to @Override tag
+    /**
+     * 
+     */
+    @Override
 	public void handle(ActionEvent event) {
 		// TODO Auto-generated method stub
 		
