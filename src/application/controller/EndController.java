6514873af32d5ad7ba24018cc27eaef2a0727fb4
oyr380 <oyr380@my.utsa.edu>
2021-11-28 19:12:45 -0600

    Added EndController, Updated Battle view and controller. Updated Room view

---
+++ src/application/controller/EndController.java
@@ -0,0 +1,26 @@
+package application.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
+
+public class EndController implements Initializable {
+
+	@FXML
+	private Text statusText;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+		if (Main.status.equals("Victory"))
+			statusText.setText("Victory!");
+
+		else
+			statusText.setText("Defeat!");
+
+	}
+}
