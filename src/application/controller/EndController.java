30d70f70a3aa4053a4a79f67857420fe162b0cf4
oyr380 <oyr380@my.utsa.edu>
2021-12-01 02:33:07 -0600

    Updated victory/defeat scene with backgrounds

--- src/application/controller/EndController.java
+++ src/application/controller/EndController.java
@@ -1,27 +1,71 @@
 package application.controller;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import application.Main;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Text;
 
 public class EndController implements Initializable {
 
 	@FXML
 	private Label statusText;
+	
+	@FXML ImageView background;
+	
+	@FXML Button retryButton;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		Image picture;
+		
+		
+		if (Main.status.equals("Victory")) {
 
-		if (Main.status.equals("Victory"))
-			statusText.setText("Victory!");
+			
+			FileInputStream input;
 
-		else
-			statusText.setText("Defeat!");
+			try {
+				input = new FileInputStream("src/images/victory.png");
+				picture = new Image(input);
+				statusText.setText("Defeat!");	
+				statusText.setText("Victory!");
+				statusText.setTextFill( Paint.valueOf("#faf402"));
+				background.setImage(picture);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+
+
+		else {
+			FileInputStream input;
+
+			try {
+				input = new FileInputStream("src/images/graveyard.png");
+				picture = new Image(input);
+				statusText.setText("Defeat!");	
+				background.setImage(picture);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+
+		}
+
+		
 
 	}
 }
