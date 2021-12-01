d7199acef71df122f9bec408e88bb5f3831c4a94
oyr380 <oyr380@my.utsa.edu>
2021-12-01 12:02:46 -0600

    Comments, New Monsters, Added Level Up, Spruced Up Attack Function

--- src/application/controller/BattleController.java
+++ src/application/controller/BattleController.java
@@ -36,39 +36,54 @@ public class BattleController implements EventHandler<ActionEvent>, Initializabl
 	@FXML
 	private Button attackButton;
 	@FXML
-	private Button defendButton;
-	@FXML
-	private Button abilityButton;
-	@FXML
 	private Button runButton;
 	@FXML
 	private Button itemsButton;
 
 	@FXML
 	void attackPressed(ActionEvent event) {
-		if (Main.monster.currHealth > 0)
-			Battle.attack(Main.playerChar, Main.monster);
-		// System.out.println(Main.monster.currHealth);
-		// System.out.println(Main.monster.level);
-		String hp = String.valueOf(Main.playerChar.currHealth);
-		playerHP.setText(hp);
-		enemyHP.setText(String.valueOf(Main.monster.currHealth));
-		playerLevel.setText(String.valueOf(Main.playerChar.level));
-		
-		//FIXME
-		//FOR THE VIDEO - force the player to die to get to the end scene
-		//Main.playerChar.currHealth = -5;
-		//Main.monster.currHealth = 500;
-		
-		if (Main.monster.currHealth <= 0) {
 
+		// Make sure monster and player are not dead.
+		if ((Main.monster.getCurrHealth() > 0) && (Main.playerChar.getCurrHealth() > 0)) {
+			Battle.attack(Main.playerChar, Main.monster);
+			playerHP.setText(String.valueOf(Main.playerChar.getCurrHealth()));
+			enemyHP.setText(String.valueOf(Main.monster.getCurrHealth()));
+		}
+
+		// Monster is dead
+		if ((Main.monster.getCurrHealth() <= 0)) {
 			try {
-				Main.playerChar.inventory.getItemList().add( "Potion" ) ;
-				//Main.playerChar.inventory.addItem("Potion");
+				//Level Up
+				Main.playerChar.setLevel(Main.playerChar.getLevel() + 1);
+				Main.playerChar.setStr(Main.playerChar.getStr() +2 );
+				Main.playerChar.setDex(Main.playerChar.getDex() +2 );
+				Main.playerChar.setDef(Main.playerChar.getDef() +2 );
+				Main.playerChar.setSpd(Main.playerChar.getSpd() +2 );
+				Main.playerChar.setCurrHealth(Main.playerChar.getCurrHealth() +5 );
+				Main.playerChar.setMaxHealth(Main.playerChar.getMaxHealth() +5 );
+				
+				//Set Monster to Defeated (Allows ability to go to next room) and return to room.
 				Dungeon.monsterDefeated = true;
 				Parent root = FXMLLoader.load(getClass().getResource("../view/Room.fxml"));
 				Main.stage.setScene(new Scene(root, 800, 800));
 				Main.stage.setTitle("The Dungeon RPG - The Dungeon");
+				Main.stage.show();
+			}
+
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		// Player is dead
+		if (Main.playerChar.currHealth <= 0) {
+			Main.status = "Defeat";
+
+			try {
+
+				Parent root = FXMLLoader.load(getClass().getResource("../view/End.fxml"));
+				Main.stage.setScene(new Scene(root, 800, 800));
+				Main.stage.setTitle("The Dungeon RPG - Defeated");
 				Main.stage.show();
 
 			}
@@ -77,49 +92,14 @@ public class BattleController implements EventHandler<ActionEvent>, Initializabl
 				e.printStackTrace();
 			}
 		}
-		
-		
-		if (Main.playerChar.currHealth <= 0) {
-			//FIXME troubleshooting print statement
-			//System.out.println("DEFEATED");
-			Main.status = "Defeat";
-		
-		try {
-
-			Parent root = FXMLLoader.load(getClass().getResource("../view/End.fxml"));
-			Main.stage.setScene(new Scene(root, 800, 800));
-			Main.stage.setTitle("The Dungeon RPG - Defeated");
-			Main.stage.show();
-
-		}
-
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	}
-	
-
-	@FXML
-	void defendPressed(ActionEvent event) {
-
-		Battle.defend(Main.playerChar, Main.monster);
-		String hp = String.valueOf(Main.playerChar.currHealth);
-		playerHP.setText(hp);
-		enemyHP.setText(String.valueOf(Main.monster.currHealth));
-	}
-
-	@FXML
-	void abilityPressed(ActionEvent event) {
-
 	}
 
 	@FXML
 	void runPressed(ActionEvent event) {
-		
+
+		// Player loses
 		Main.status = "Defeat";
-		
+
 		try {
 
 			Parent root = FXMLLoader.load(getClass().getResource("../view/End.fxml"));
@@ -136,6 +116,8 @@ public class BattleController implements EventHandler<ActionEvent>, Initializabl
 
 	@FXML
 	void itemsPressed(ActionEvent event) {
+		
+		// Player views inventory
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("../view/Inventory.fxml"));
 			Main.stage.setScene(new Scene(root, 800, 800));
@@ -157,20 +139,59 @@ public class BattleController implements EventHandler<ActionEvent>, Initializabl
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-		String hp = String.valueOf(Main.playerChar.currHealth);
-		playerHP.setText(hp);
-		playerName.setText(Main.playerChar.name);
 
+		// Updates the view to show current player information
+		playerHP.setText(String.valueOf(Main.playerChar.getCurrHealth()));
+		playerName.setText(Main.playerChar.getName());
 		playerLevel.setText(String.valueOf(Main.playerChar.level));
 
-		if (Dungeon.curMonster.equals("Skeleton")) {
-			Main.monster = new Player("Skeleton", "noClass", 5, 5, 5, 5, 5, 5, 5, 5, null);
+		// Updates the view to show current monster information
+		switch (Dungeon.curMonster) {
+
+		case "Skeleton":
+			Main.monster = new Player("Skeleton", "noClass", 5, 5, 1, 5, 5, 5, 5, 5, null);
 			enemyLevel.setText(String.valueOf(Main.monster.getLevel()));
 			enemyName.setText(Main.monster.getName());
 			enemyHP.setText(String.valueOf(Main.monster.getCurrHealth()));
+			break;
 
+		case "Giant Spider":
+			Main.monster = new Player("Giant Spider", "noClass", 5, 5, 2, 5, 10, 10, 10, 10, null);
+			enemyLevel.setText(String.valueOf(Main.monster.getLevel()));
+			enemyName.setText(Main.monster.getName());
+			enemyHP.setText(String.valueOf(Main.monster.getCurrHealth()));
+			break;
+
+		case "Lizard Warrior":
+			Main.monster = new Player("Lizard Warrior", "noClass", 5, 5, 3, 5, 13, 13, 13, 13, null);
+			enemyLevel.setText(String.valueOf(Main.monster.getLevel()));
+			enemyName.setText(Main.monster.getName());
+			enemyHP.setText(String.valueOf(Main.monster.getCurrHealth()));
+			break;
+
+		case "Water Demon":
+			Main.monster = new Player("Water Demon", "noClass", 5, 5, 5, 4, 15, 15, 15, 15, null);
+			enemyLevel.setText(String.valueOf(Main.monster.getLevel()));
+			enemyName.setText(Main.monster.getName());
+			enemyHP.setText(String.valueOf(Main.monster.getCurrHealth()));
+			break;
+
+		case "Flame Warrior":
+			Main.monster = new Player("Flame Warrior", "noClass", 5, 5, 5, 5, 18, 18, 18, 18, null);
+			enemyLevel.setText(String.valueOf(Main.monster.getLevel()));
+			enemyName.setText(Main.monster.getName());
+			enemyHP.setText(String.valueOf(Main.monster.getCurrHealth()));
+			break;
+
+		case "Dragon Knight":
+			Main.monster = new Player("Dragon Knight", "noClass", 5, 5, 10, 5, 20, 20, 20, 20, null);
+			enemyLevel.setText(String.valueOf(Main.monster.getLevel()));
+			enemyName.setText(Main.monster.getName());
+			enemyHP.setText(String.valueOf(Main.monster.getCurrHealth()));
+			break;
+
+		default:
+			break;
 		}
 	}
-
 }
